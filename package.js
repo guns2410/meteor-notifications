735c25866ad31a6cafba8b1a705b6c6e23b80296
Guns <guns2410@users.noreply.github.com>
2016-03-04 09:37:38 +0530

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'gunjansoni:notifications',
-	version: '0.1.0',
+	version: '0.1.2',
 	summary: 'Exchange Notification Messages between Server and Clients via DDP Connection',
 	git: 'https://github.com/guns2410/meteor-notifications',
 	documentation: 'README.md'
