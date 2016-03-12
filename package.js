bf537f0bf9de76ee325cc3ee403c046637b6e84a
Guns <guns2410@users.noreply.github.com>
2016-03-12 14:37:57 +0530

    updated ddpconnector package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'gunjansoni:notifications',
-	version: '0.1.9',
+	version: '0.2.0',
 	summary: 'Exchange Notification Messages between Server and Clients via DDP Connection',
 	git: 'https://github.com/guns2410/meteor-notifications',
 	documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.2.1');
-	api.use(['ecmascript', 'mongo', 'underscore', 'jsx@0.2.3', 'gunjansoni:ddpconnector@0.0.1']);
+	api.use(['ecmascript', 'mongo', 'underscore', 'jsx@0.2.3', 'gunjansoni:ddpconnector@0.1.0']);
 	api.addFiles('notifications-client.jsx');
 	api.addFiles('notifications-server.jsx', 'server');
 	api.export(['ClientNotifications', 'ServerNotifications']);
